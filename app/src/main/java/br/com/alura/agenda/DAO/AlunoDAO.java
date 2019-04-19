45e7cd2991fed4195d6d56584d807ada268bb770
Victor Ramos <victor.ramosoliveira@outlook.com>
2019-04-19 18:14:53 -0300

    Adicionar edição de alunos

--- app/src/main/java/br/com/alura/agenda/DAO/AlunoDAO.java
+++ app/src/main/java/br/com/alura/agenda/DAO/AlunoDAO.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,14 +34,20 @@ public class AlunoDAO extends SQLiteOpenHelper {
     public void insere(Aluno aluno) {
         SQLiteDatabase db = getWritableDatabase();
 
+        ContentValues dados = pegaDadosAluno(aluno);
+        db.insert("Alunos", null, dados);
+
+    }
+
+    @NonNull
+    private ContentValues pegaDadosAluno(Aluno aluno) {
         ContentValues dados = new ContentValues();
         dados.put("nome", aluno.getNome());
         dados.put("endereco", aluno.getEndereco());
         dados.put("telefone", aluno.getTelefone());
         dados.put("site", aluno.getSite());
         dados.put("nota", aluno.getNota());
-        db.insert("Alunos", null, dados);
-
+        return dados;
     }
 
     public List<Aluno> buscaAlunos() {
@@ -71,5 +78,14 @@ public class AlunoDAO extends SQLiteOpenHelper {
         String [] params = {aluno.getId().toString()};
 
         db.delete("Alunos", "id = ?" , params);
+    }
+
+    public void altera(Aluno aluno) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues dados = pegaDadosAluno(aluno);
+
+        String[] params ={aluno.getId().toString()};
+        db.update("Alunos", dados, "id = ? ", params);
     }
 }
--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -42,8 +42,14 @@ public class FormularioActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.menu_formulario_ok:
                 Aluno aluno = helper.pegaAluno();
+
                 AlunoDAO dao = new AlunoDAO(this);
-                dao.insere(aluno);
+                if (aluno.getId() != null){
+                    dao.altera(aluno);
+                } else {
+                    dao.insere(aluno);
+
+                }
                 dao.close();
 
                 Toast.makeText(FormularioActivity.this, "Aluno: " + aluno.getNome() + " salvo!" , Toast.LENGTH_SHORT).show();
